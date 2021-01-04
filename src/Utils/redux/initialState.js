6f82b6f871d4db663d673df575304d4ff8705c6f
Joel <jbirch88655@gmail.com>
2021-01-03 20:56:12 -0800

    updated

--- src/Utils/redux/initialState.js
+++
@@ -1,8 +0,0 @@
-const somethingIsLoading = { loading: false }
-//const supportIssues = { loading: true, myIssues: {}, globalIssues: {} }
-const initialState = {
-//  supportIssues,
-  somethingIsLoading,
-}
-
-export default initialState
--- src/Utils/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux"
-import loadingReducer from "./loading"
-const rootReducer = combineReducers({
-  somethingIsLoading: loadingReducer
-})
-
-export default rootReducer
